282e3ca86cbd593a4056fc73b2e7c7758123af99
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-09-10 14:02:03 -0500

    Implemented variables functionality

--- variables.js
+++ variables.js
@@ -1,17 +1,26 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+//CODE HERE
+console.log("//////////////////PROBLEM 1////////////////////");
+let myName = "Alfonso Arriaga";
+console.log(myName);
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+console.log("//////////////////PROBLEM 2////////////////////");
+let faveNum = 12;
+console.log(faveNum);
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+console.log("//////////////////PROBLEM 3////////////////////");
+let lovesCode = true;
+console.log(`lovesCode: ${lovesCode}`);
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +30,13 @@
 */
 
 //CODE HERE
+console.log("//////////////////PROBLEM 4////////////////////");
+if (faveNum === 13) {
+  console.log("Lucky 13!")
+}else{
+  console.log("Not lucky 13");
+}
+
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +44,7 @@
 */
 
 //CODE HERE
+console.log("//////////////////PROBLEM 5////////////////////");
+for (let i = 0; i < 5; i++) {
+  console.log(`${i+1} - ${faveNum}`);
+}
